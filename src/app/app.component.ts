3c16d7c528fdc4140db30bab45727b2ab75e47c2
AlanDPA <alanag1303@gmail.com>
2025-05-07 16:00:20 -0500

    1.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { AlertaBienvenidaComponent } from "./alerta-bienvenida/alerta-bienvenida.component";
 import { PreguntasComponent } from "./preguntas/preguntas.component";
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [AlertaBienvenidaComponent, PreguntasComponent],
+  imports: [AlertaBienvenidaComponent, PreguntasComponent, BrowserModule, RouterModule],
   templateUrl: './app.component.html',
 
 })
